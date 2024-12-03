e49342713e25e744c3524c10ad4d85c9619488a2
ORTAngelo <161719156+ORTAngelo@users.noreply.github.com>
2024-12-03 20:13:58 +0700

    Add files via upload

--- WebDevFinal/Database/database.php
+++ WebDevFinal/Database/database.php
@@ -1,1 +1,6 @@
-
+<?php
+$db_host = 'localhost'; // Host name
+$db_name = 'octo_thaiparts'; // Database name
+$db_username = 'root'; // Database username
+$db_password = ''; // Database password
+?>
